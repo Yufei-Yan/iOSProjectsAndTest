84e410a79b3afe26c91cc1bcddc2b132eb24c257
Yufei-Yan <yanyufei11111@163.com>
2016-11-08 01:53:52 -0600

    Initial commit with codes of Chapter 4: More User Interface Fun

---
+++ Ch4_test/Ch4_test/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  Ch4_test
+//
+//  Created by Yan on 9/27/16.
+//  Copyright © 2016 Yan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var numberField: UITextField!
+    @IBOutlet weak var sliderLabel: UILabel!
+
+    @IBOutlet weak var leftSwitch: UISwitch!
+    @IBOutlet weak var rightSwitch: UISwitch!
+ 
+    @IBOutlet weak var doSomethingButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        sliderLabel.text = "50"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func textFieldDoneEditing(sender: UITextField) {
+        sender.resignFirstResponder()
+    }
+
+
+    @IBAction func onTapGestureRecognized(_ sender: AnyObject) {
+        nameField.resignFirstResponder()
+        numberField.resignFirstResponder()
+    }
+    
+    @IBAction func onSliderChanged(_ sender: UISlider) {
+        sliderLabel.text = "\(lroundf(sender.value))"
+    }
+    
+    @IBAction func onSwitchChanged(_ sender: UISwitch) {
+        let setting = sender.isOn
+        leftSwitch.setOn(setting, animated: true)
+        rightSwitch.setOn(setting, animated: true)
+    }
+
+    @IBAction func toggleControls(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            leftSwitch.isHidden = false
+            rightSwitch.isHidden = false
+            doSomethingButton.isHidden = true
+        } else {
+            leftSwitch.isHidden = true
+            rightSwitch.isHidden = true
+            doSomethingButton.isHidden = false
+        }
+    }
+    
+    @IBAction func onButtonPressed(_ sender: UIButton) {
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# iOSProjectsAndTest
+
+All test codes in this repo are adapted from the book *Beginning iPhone Development with Swift 2* in Swift 3.  
+All projects are developped for iOS Development course at [Auburn University](http://www.auburn.edu).
