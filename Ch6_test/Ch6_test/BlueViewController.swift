d60850b55c73008568f2b19328ceaeebbc04c282
Yufei-Yan <yanyufei11111@163.com>
2016-11-17 14:04:42 -0600

    Project in Chapter 6

---
+++ Ch6_test/Ch6_test/BlueViewController.swift
@@ -0,0 +1,43 @@
+//
+//  BlueViewController.swift
+//  Ch6_test
+//
+//  Created by Yan on 10/26/16.
+//  Copyright © 2016 Yan. All rights reserved.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func blueButtonPressed(sender: UIButton) {
+        let alert = UIAlertController(title: "Blue View Button Pressed",
+                                      message: "You pressed the button on the blue view",
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: "Yep, I did", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Ch6_test/Ch6_test/YellowViewController.swift
@@ -0,0 +1,44 @@
+//
+//  YellowViewController.swift
+//  Ch6_test
+//
+//  Created by Yan on 10/26/16.
+//  Copyright © 2016 Yan. All rights reserved.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func blueButtonPressed(sender: UIButton) {
+        let alert = UIAlertController(title: "Yellow View Button Pressed",
+                                      message: "You pressed the button on the yellow view",
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: "Yep, I did", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
